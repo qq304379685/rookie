35498483cc4621e535eaae5b13ef09439b5e07ee
xushuo <304379685@qq.com>
2017-03-07 23:32:18 +0800

    更改为war的形式

---
+++ rookie-web/src/main/java/web/controller/TestController.java
@@ -0,0 +1,17 @@
+package web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by xushuo on 2017/3/7.
+ */
+@Controller
+@RequestMapping("test")
+public class TestController {
+
+
+}
